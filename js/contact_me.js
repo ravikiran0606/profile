aab32902619a3545e185c7b1b43a3eadf3552503
ravikiran0606 <sravikiran0606@gmail.com>
2018-05-06 01:26:56 +0530

    Add Blog Files

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://getsimpleform.com/messages?form_api_token=23c1ec3e931a5b35b101a18701c737e7",
         type: "POST",
         data: {
           name: name,
@@ -42,12 +42,14 @@ $(function() {
           $('#contactForm').trigger("reset");
         },
         error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
